2e9479f0f48a3bec264a4ab0827568c504f9844c
medi.demirdelen <medi.demirdelen@gmail.com>
2022-10-28 15:20:48 +0200

    change style

--- js/index.js
+++ js/index.js
@@ -9,7 +9,6 @@ const fetchMovies = async () => {
   movies = await fetch(
     `https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=${search}`
   ).then((res) => res.json());
-  console.log(movies);
 };
 
 const moviesDisplay = async () => {
